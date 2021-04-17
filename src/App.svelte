6aaa07a778d4873a8e79303715d1f437176322ae
Steel Feel <himankjain@yahoo.in>
2021-04-17 15:44:31 +0530

    Template page

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+//	export let name;
+	import Header from './Header.svelte';
+
+	let sByte32="adasd", sRawString="" ;
+
+	function fnCalcbyte32(){
+
+	}
+
+</script>
+
+<main class="font-sans">
+	<Header />
+
+	<section class="m-auto p-6 lg:py-4 lg:px-16" >
+		<p class="text-lg" >Input the String value to be converted into byte32</p>
+			<div class="flex flex-col" >
+				<div class="flex items-center flex-col md:flex-row " >
+					<textarea bind:value={sRawString} rows="4" cols="60" class="max-w-lg shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md" type="text"></textarea>
+					<a on:click={fnCalcbyte32} class="self-center font-bold cursor-pointer m-4 p-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" >
+						Calculate
+					</a>
+				</div>
+				
+
+				
+				<div class="my-8 " >
+					<p class="p-6 border-solid border-2 border-indigo-500" >{sByte32}</p>
+				</div>
+			</div>
+	</section>
+
+</main>
+
+<style>
+
+</style>
---
+++ src/Header.svelte
@@ -0,0 +1,61 @@
+<script>
+  //  export let current;
+    let bmenuOpen = false;
+
+    var openMenu = () => {
+        bmenuOpen = !bmenuOpen;
+    };
+
+    var closeMenu = () => {
+        bmenuOpen = !bmenuOpen;
+    };
+
+    //  class:w-2&sol;3={bmenuOpen}
+</script>
+<style>
+
+nav.visible-m
+{
+    width: 66.6%;
+    opacity: 1;
+    visibility: visible;
+}
+
+
+
+
+
+
+</style>
+<div class="sm:p-8 p-2 bg-grey-100">
+    <div class="p-1 flex place-content-between md:grid md:grid-cols-3">
+        <a href="#"
+            ><img class="w-40 h-12" src="assests/logo2.svg" alt="Logo" /></a
+        >
+        <img
+            on:click={openMenu}
+            class="md:hidden w-6 h-6 cursor-pointer"
+            src="assests/menu.svg"
+            alt="Open Navigation"
+        />
+
+
+        <nav   class="md:flex md:static md:justify-end md:bg-transparent md:z-0 md:flex-row-reverse w-0 md:w-full opacity-0 md:opacity-100 h-screen md:h-auto z-50 p-2 transition-all ease-in fixed top-0 right-0 bg-gray-100"
+                class:visible-m={bmenuOpen} >
+            <img
+                on:click={closeMenu}
+                class="md:hidden m-2 cursor-pointer float-right"
+                src="assests/close-icon.svg"
+                alt="Close Navigation"
+            />
+            <ul class="list-none md:flex m-3">
+                <!-- Shift+Alt+[down-arrow-key] for replicating line-->
+                <li class="font-bold">
+                    <a on:click={closeMenu} href="#main">Home</a>
+                </li>
+                <!-- <li><a on:click={closeMenu} href="#work">My Work</a></li>
+                <li><a on:click={closeMenu} href="#contact">Contact</a></li> -->
+            </ul>
+        </nav>
+    </div>
+</div>
